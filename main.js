bba2b8edf92b578726875a4566261f58052d07c9
Bartosz Gronkiewicz <pasiak5@gmail.com>
2016-02-06 17:56:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+   		$(this).animate({
+    		left: '-30px',
+    	});
+    	$('header').animate({
+    		left: '0'
+    	});
+    });
+	if($(window).width < 1000) {
+    $('article').click(function() {
+    	$('header').animate({
+    		left: '-301px'
+    	});
+    	$('.menu-btn').animate({
+    		left: '0'
+    	});
+    });
+    }
+
+    $('.info-btn').click(function(){
+        if($(this).css('opacity') == 0.7) {
+            $(this).css('opacity', '1');
+        }
+        else {
+            $(this).css('opacity', 0.7);
+        }
+    	$('.icon-info-circled').toggleClass('active');
+    	if($('.icon-info-circled').hasClass('active')){
+	    	$('.contact-info').animate({
+	    		right: '0'
+	    	},200);
+	    }
+	    else{
+	    	$('.contact-info').animate({
+	    		right: '-280px'
+	    	},200);
+	    }
+    });
+
+    $('.width-span').html($('.photo-box').width());
+    $('.height-span').html($('.photo-box').height());
+    $(window).on('resize', function(){
+    	$('.width-span').html($('.photo-box').width());
+    	$('.height-span').html($('.photo-box').height());
+    });
+    
+    $('.width-span').html($('.post .header').width());
+    $('.height-span').html($('.post .header').height());
+    $(window).on('resize', function(){
+    	$('.width-span').html($('.post .header').width());
+    	$('.height-span').html($('.post .header').height());
+    });
+    
+    for(var i = 0; i < 16; i++) {
+	    $(".photo-box").children().eq(i).hover(function(){
+	    	$(this).find(".find-out").css("display", "flex");
+	        }, function(){
+	        $(this).find(".find-out").css("display", "none");
+            });
+    }
+    $('.photo-box:first-child').hover(function() {
+        $('.photo-box:after').css("display", "flex");
+        }, function(){
+	    $('.photo-box:after').css("display", "none");
+    });
+    
+    $('.galery').hover(function() {
+        $(this).find('i').hide();
+    });
+});
